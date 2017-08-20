f3278fcf9ea87257f72ce54b4114e5acc87dbf23
Administrador <edgar.yerena@metodika.mx>
2017-08-20 17:24:38 -0500

    Header initial construction

---
+++ partials/header.php
@@ -0,0 +1,21 @@
+<header class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- logo -->
+  <a class="navbar-brand" href="index.php">Logo</a>
+  <!-- hamburguer menu icon -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- nav menu -->
+  <nav class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a href="index.php" class="nav-link">Inicio</a>
+      </li>
+
+      <li class="nav-item">
+        <a href="contacto.php" class="nav-link">Contacto</a>
+      </li>
+    </ul>
+  </nav>
+</header>
